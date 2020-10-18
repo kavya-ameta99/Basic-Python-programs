b7aec896fd142c0a064926dc92371967f2c95361
kavya-ameta99 <72993242+kavya-ameta99@users.noreply.github.com>
2020-10-18 18:33:47 +0530

    Changes made
    Updated it to make it easy to understand. Good work though!

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,20 +1,23 @@
+#importing library
 import sys
 
-#Declaring satic list
-#Can be changed to dynamic in futher modification easily
+#Declaring the satic list
+#Can be changed to dynamic in further modification easily
 A = [64, 25, 12, 22, 11]
 
 #Tansversing through the list
 for i in range(len(A)):
-    #Here we need to find the smallest element in the dynamic defined range
+#Here we need to find the smallest element in the defined range
     minimum_index = i
     for j in range(i+1, len(A)):
+ #checking the test condition
         if A[minimum_index] >A[j]:
             minimum_index = j
-
-    #Swapping the elements
+            
+ #Swapping the elements
     A[i], A[minimum_index] = A[minimum_index], A[i]
-
+  
+  #printing the results
 print("Initial array is: ")
 print(A)
 print("The sorted array is: ")
